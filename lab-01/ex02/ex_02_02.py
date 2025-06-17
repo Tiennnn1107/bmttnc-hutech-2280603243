f738d58be3222f2fcc61f72d721124044ac9df19
Tiennnn1107 <tantienlk2020@gmail.com>
2025-06-17 13:59:21 +0700

    Buoi 5

--- lab-01/ex02/ex_02_02.py
+++
@@ -1,3 +0,0 @@
-ban_kinh = float(input("Nhap ban kinh :"))
-dien_tich = 3.14 *(ban_kinh**2)
-print("Dien tich hinh tron =",dien_tich)
--- lab-01/ex02/ex_02_03.py
+++
@@ -1,5 +0,0 @@
-so=int (input("Nhap mot so nguyen:"))
-if so % 2 ==0:
-    print("So chan")
-else:
-    print("khong phai so chan")
